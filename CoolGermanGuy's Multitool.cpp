17307d8a1673ba09b190479ac59839598e3d5a8b
CoolGermanGuy <88776295+CGGonGitHub@users.noreply.github.com>
2024-08-28 22:18:41 +0200

    Create CoolGermanGuy's Multitool.cpp

---
+++ CoolGermanGuy's Multitool.cpp
@@ -0,0 +1,10 @@
+using namespace std;
+#include <iostream>
+#include "functions.h"
+
+int main()
+{
+	//std::cout << "\033[95m" << "Welcome to CoolGermanGuy's Multitool" << "\033[0m" << "\n" << std::endl;
+	//std::cout << "\033[95mWelcome to CoolGermanGuy's Multitool\033[0m\n";
+	menu();
+}
